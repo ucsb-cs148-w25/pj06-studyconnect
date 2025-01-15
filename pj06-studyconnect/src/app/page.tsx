cd65703813b8310dc317133b1ec13f8e027935de
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2025-01-15 12:00:29 -0800

    basic header/footer

--- pj06-studyconnect/src/app/page.tsx
+++ pj06-studyconnect/src/app/page.tsx
@@ -1,9 +1,17 @@
+import React from 'react';
+
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col justify-between items-center h-screen">
+      <header>
+        <h1>Header</h1>
+      </header>
       <div>
         <p>Hello World</p>
       </div>
-    </>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </div>
   );
 }
