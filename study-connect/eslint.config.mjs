a70baf79740ed2a06b36af7184807246a8663622
hannuhsu <yasu@ucsb.edu>
2025-02-03 15:57:19 -0800

    eslint.config.mjs

--- study-connect/eslint.config.mjs
+++ study-connect/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { rule } from "postcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,8 +12,11 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rule: {
-      "no-unused-vars": "off"
+    rules: {
+      "no-unused-vars": "off",
+      "prefer-const": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     }
   }
 ];
