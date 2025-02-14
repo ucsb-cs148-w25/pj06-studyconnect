f56af851bbb74d8c61d3a91e563ab44dedb1f415
Allen Hu <hu16@umail.ucsb.edu>
2025-02-13 22:09:14 -0800

    Delete the redundant import in ClassForum page

--- study-connect/app/components/ClassForum.tsx
+++ study-connect/app/components/ClassForum.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { createPost } from '../actions/postActions';
 import Link from 'next/link';
 import { createPost, fetchPosts } from '../actions/postActions';
 
