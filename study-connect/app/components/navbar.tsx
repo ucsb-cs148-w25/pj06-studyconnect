dea8648990f3dc1218359514bdbcd2adfe00c7fe
jinanthony <anthonyjin@ucsb.edu>
2025-03-03 15:13:44 -0800

    fix typo

--- study-connect/app/components/navbar.tsx
+++ study-connect/app/components/navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Link from 'next/link';gi
+import Link from 'next/link';
 import Image from "next/image";
 import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
