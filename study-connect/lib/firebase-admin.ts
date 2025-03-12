7b4d33972be462c8e5e1f73c2f492401af46a965
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2025-03-12 14:31:25 -0700

    Revert "Add upload image feature to forum posts"
    This reverts commit 6be1dfd384c624826a2b2357a50b865de7421da1.

--- study-connect/lib/firebase-admin.ts
+++ study-connect/lib/firebase-admin.ts
@@ -1,6 +1,5 @@
-import { initializeApp, getApps, cert, getApp } from 'firebase-admin/app';
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import { getStorage } from "firebase-admin/storage";
 
 if (!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
   throw new Error('Firebase Admin environment variables are missing');
@@ -19,4 +18,3 @@ if (!getApps().length) {
 }
 
 export const db = getFirestore();
-export const storage = getStorage();
